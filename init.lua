eb45637f147567a00f2f4181509fd2929f3658d8
flower-of-the-bridges <gfiordeponti@gmail.com>
2025-04-23 09:32:37 +0200

    show errors

--- init.lua
+++ init.lua
@@ -171,7 +171,10 @@ vim.opt.confirm = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>e', function()
+  vim.diagnostic.setqflist()
+  vim.cmd('copen')
+end, { desc = 'Show all [E]rrors in quickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
